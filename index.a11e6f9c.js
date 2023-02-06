02bc780301e803eda91b51aeab5f2f0dbfa57aef
JaneVanilla <janevanilla92@gmail.com>
2023-02-06 18:00:49 +0000

    Deploying to gh-pages from @ JaneVanilla/goit-js-hw-11@84c8135e29fa322bf10076697a68bf625e4717d6 🚀

---
+++ index.a11e6f9c.js
@@ -0,0 +1,2 @@
+var result=fetch("https://pixabay.com/api/?key=33420442-09e22f34ecac941bb7ddcda2e&q=yellow+flowers&image_type=photo").then((function(e){return e.json()})).then((function(e){return console.log(e)}));console.log(result);
+//# sourceMappingURL=index.a11e6f9c.js.map
